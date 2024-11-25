8225c1a22f450e46ca6ea17ea6bb888599af3108
mirnatul <www.mirnatul@gmail.com>
2024-11-25 13:23:56 +0600

    seco

--- forntend/TourAssistant/src/App.jsx
+++ forntend/TourAssistant/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1 className="font-bold felx justify-center text-center w-full h-full">Root Route</h1>} />
         {/* Demo rou */}
+        {/* second demo */}
       </Routes>
 
     </BrowserRouter>
