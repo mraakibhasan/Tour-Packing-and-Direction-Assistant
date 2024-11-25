f6f5ad5deeaea5614b60d2b7109b07d6883d3e0a
mirnatul <www.mirnatul@gmail.com>
2024-11-25 13:20:55 +0600

    lipon

--- forntend/TourAssistant/src/App.jsx
+++ forntend/TourAssistant/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<h1 className="font-bold felx justify-center text-center w-full h-full">Root Route</h1>}/>
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<h1 className="font-bold felx justify-center text-center w-full h-full">Root Route</h1>} />
+        {/* Demo rou */}
+      </Routes>
+
     </BrowserRouter>
   )
 }
