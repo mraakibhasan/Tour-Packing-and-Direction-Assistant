f1791c8f85ad32858f870b17e93c31a9d40d3887
Tuhin Hosean <43568783+azimhossaintuhin@users.noreply.github.com>
2024-11-26 14:02:14 +0600

    Revert "Revert 4 tuhin/frontend/softfix"

---
+++ forntend/TourAssistant/src/components/Modal.jsx
@@ -0,0 +1,6 @@
+
+const  Modal =  ()=>{
+    return(
+        <h1>Modal</h1>
+    )
+}
---
+++ forntend/TourAssistant/src/pages/Bagpack/index.jsx
@@ -0,0 +1,13 @@
+import { useState , useEffect , useRef } from "react";
+
+const BackPack = ()=>{
+    
+    // ====== components ====== //
+    return(
+        <div>
+        <h1>
+        
+        </h1>
+        </div>
+    )
+}
