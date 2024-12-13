e92904c9fd1874a6777314a7b7787ff6d805112a
mirnatul <www.mirnatul@gmail.com>
2024-12-13 15:32:07 +0600

    chocolate selecting page created

--- forntend/TourAssistant/src/App.jsx
+++ forntend/TourAssistant/src/App.jsx
@@ -5,6 +5,8 @@ import Register from "./pages/Register";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import HomePage from "./pages/Home/HomePage";
+import LoveCalculator from './pages/LoveCalculator';
+import Chocolate from "./pages/Chocolate/Chocolate";
 function App() {
 
 
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login/" element={<LoginScreen />} />
           <Route path="/register/" element={<Register />} />
+          <Route path="/love-calculator/" element={<LoveCalculator />} />
+          <Route path="/chocolate/" element={<Chocolate />} />
         </Routes>
 
       </BrowserRouter>
--- forntend/TourAssistant/src/pages/Home/components/Banner.jsx
+++ forntend/TourAssistant/src/pages/Home/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import bannerImg from '../../../assets/banner-pic.png' //
 import cloud from '../../../assets/pink-cloud.webp' //
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -13,7 +14,7 @@ const Banner = () => {
                     <img src={cloud} alt="" />
                 </div>
             </div>
-            <div className="bg-white min-h-screen flex flex-col items-center px-4 sm:px-6 lg:px-8">
+            <div className="bg-white flex flex-col items-center px-4 sm:px-6 lg:px-8">
                 <div className="max-w-2xl space-y-6 text-center">
                     <div>
                         <h2 className="mt-6 text-5xl font-bold text-gray-900">Find Your <span className='text-red-400'>Real Connections</span></h2>
@@ -23,12 +24,13 @@ const Banner = () => {
                         </p>
                     </div>
                     <div className='flex justify-center'>
-                        <button
+                        <Link
+                            to={`/chocolate`}
                             type="button"
                             className="shadow-[3px_3px_0px_black] relative px-6 py-3 text-sm font-medium text-black bg-[rgb(255,180,180)] border-2 border-black hover:bg-red-300 hover:scale-105 hover:transition-all"
                         >
                             Join MyDate
-                        </button>
+                        </Link>
                     </div>
                 </div>
                 <div className='mt-6'>
